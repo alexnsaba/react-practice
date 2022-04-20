b45e3c4920fd2be9bf191f3785f18d9b66472178
Niyonsaba Alex <alexnewzniyo@gmail.com>
2022-04-20 16:59:21 +0300

    add properties to components

---
+++ src/components/ComponentDemo.js
@@ -0,0 +1,11 @@
+function ComponentDemo(props){
+    console.log(props)
+    return(
+        <div>
+            <h1>Monday</h1>
+            <h1>Tuesday</h1>
+            <h1>Wednesday</h1>
+        </div>
+    );
+}
+export default ComponentDemo;
---
+++ src/components/ComponentDemoTwo.js
@@ -0,0 +1,11 @@
+function ComponentDemoTwo(props){
+    return(
+        <div>
+            <h1>Today is {props.day}</h1>
+            <h2>Main</h2>
+            <h3>Footer</h3>
+        </div>
+    );
+}
+
+export default ComponentDemoTwo;
